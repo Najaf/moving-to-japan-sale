92db2609959f32275b3755c5f396a9e51923e6fe
Najaf Ali <ali.najaf@gmail.com>
2018-05-13 12:58:35 +0100

    Made a list of items

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require 'sinatra'
+require 'yaml'
+require 'ostruct'
+
+def items
+  @items ||= YAML.load(File.read('items.yaml')).map do |item|
+    OpenStruct.new(item)
+  end
+end
+
+def item_with_id(id)
+  items.select { |item| item.id == id }
+end
 
 get '/' do
   haml :index
